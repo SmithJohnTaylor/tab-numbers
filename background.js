08e324c1987fc59ae4797815950af360cddbf9f4
John-Taylor Smith <john-taylor.smith@alfresco.com>
2018-04-09 08:52:16 -0400

    trying to fix numbering not persisting

--- background.js
+++ background.js
@@ -1,29 +1,23 @@
 var separator = " "
 var total = 0
+var enabled = true
 
 function indexTitle(index, title, max) {
 
   var start = title.indexOf(separator) > -1 ? (title.indexOf(separator) + separator.length) : -1
   var originalTitle = title.substring(start)
 
-  if (index > 7 && index == max - 1) {
-    return "9".concat(separator, title)
-  } else if (index > 7) {
-    return "...".concat(separator, title)
-  } else {
-    return "".concat((index + 1).toString(), separator, originalTitle)
-  }
+  if (enabled == false) { return originalTitle }
+
+  if (index > 7 && index == max - 1) { return "9".concat(separator, title) }
+  else if (index > 7) { return "...".concat(separator, title) }
+  else { return "".concat((index + 1).toString(), separator, originalTitle) }
 }
 
 function isValid(url) {
-  valid = true
   prefixes = ["chrome://", "about:", "view-source:"]
-  for (var i = 0; i < prefixes.length; i++) {
-    if (url.lastIndexOf(prefixes[i]) == 0) {
-      valid = false
-    }
-  }
-  return valid
+  for (var i = 0; i < prefixes.length; i++) { if (url.lastIndexOf(prefixes[i]) == 0) { return false } }
+  return true
 }
 
 function tabs() {
@@ -34,17 +28,20 @@ function tabs() {
         chrome.tabs.executeScript(tabs[i].id, { code: "chrome.runtime.sendMessage({'index': ".concat(tabs[i].index, ", 'id': ", tabs[i].id, ", 'title': '", tabs[i].title, "' })") })
       }
     }
-  })
+  }
+  )
 }
 
 chrome.commands.onCommand.addListener(function (command) {
+  enabled = true
   tabs()
 })
 chrome.tabs.onActivated.addListener(function (tab) {
-  tabs()
+  if (enabled) {
+    //enabled = false
+    tabs()
+  }
 })
 chrome.runtime.onMessage.addListener(function (message, sender) {
-  chrome.tabs.executeScript(message.id, {
-    code: "document.title=".concat('"', indexTitle(message.index, message.title, total), '"')
-  })
+  chrome.tabs.executeScript(message.id, { code: "document.title=".concat('"', indexTitle(message.index, message.title, total), '"') })
 })
